5f08c32b87d6366d3c05f2acdfd3d52dc53958aa
Preston-Crary <preston.crary@liferay.com>
2019-07-17 13:52:28 -0700

    LPS-98197 Avoid calling validateFile() since fetchByG_F_T() flushes the current session causing the DLFileEntry to become stale ni moveFileEntryImpl().

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -2074,22 +2074,7 @@ public class DLFileEntryLocalServiceImpl
 			String title)
 		throws PortalException {
 
-		if (folderId != DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
-			DLFolder parentDLFolder = dlFolderPersistence.findByPrimaryKey(
-				folderId);
-
-			if (groupId != parentDLFolder.getGroupId()) {
-				throw new NoSuchFolderException();
-			}
-		}
-
-		DLFolder dlFolder = dlFolderPersistence.fetchByG_P_N(
-			groupId, folderId, title);
-
-		if (dlFolder != null) {
-			throw new DuplicateFolderNameException(
-				"A folder already exists with name " + title);
-		}
+		_validateFolder(groupId, folderId, title);
 
 		DLFileEntry dlFileEntry = dlFileEntryPersistence.fetchByG_F_T(
 			groupId, folderId, title);
@@ -2457,9 +2442,8 @@ public class DLFileEntryLocalServiceImpl
 
 		long oldDataRepositoryId = dlFileEntry.getDataRepositoryId();
 
-		validateFile(
-			dlFileEntry.getGroupId(), newFolderId, dlFileEntry.getFileEntryId(),
-			dlFileEntry.getFileName(), dlFileEntry.getTitle());
+		_validateFolder(
+			dlFileEntry.getGroupId(), newFolderId, dlFileEntry.getTitle());
 
 		dlFileEntry.setFolderId(newFolderId);
 		dlFileEntry.setTreePath(dlFileEntry.buildTreePath());
@@ -3058,6 +3042,27 @@ public class DLFileEntryLocalServiceImpl
 		// Latest file version
 
 		removeFileVersion(dlFileEntry, latestDLFileVersion);
+	}
+
+	private void _validateFolder(long groupId, long folderId, String title)
+		throws PortalException {
+
+		if (folderId != DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			DLFolder parentDLFolder = dlFolderPersistence.findByPrimaryKey(
+				folderId);
+
+			if (groupId != parentDLFolder.getGroupId()) {
+				throw new NoSuchFolderException();
+			}
+		}
+
+		DLFolder dlFolder = dlFolderPersistence.fetchByG_P_N(
+			groupId, folderId, title);
+
+		if (dlFolder != null) {
+			throw new DuplicateFolderNameException(
+				"A folder already exists with name " + title);
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
