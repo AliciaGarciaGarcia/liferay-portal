a23e1b2c70b8f4f452bff7a5f7ec4ecf67130ff7
Alicia Garcia <alicia.garcia@liferay.com>
2020-10-19 17:19:44 +0200

    LPS-122144 TranslationWebConstants

---
+++ modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/constants/TranslationWebConstants.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.translation.web.internal.constants;
+
+/**
+ * @author Alicia Garcia
+ */
+public class TranslationWebConstants {
+
+	public static final String AVAILABLE_SOURCE_LANGUAGE_IDS =
+		"AVAILABLE_SOURCE_LANGUAGE_IDS";
+
+	public static final String AVAILABLE_TARGET_LANGUAGE_IDS =
+		"AVAILABLE_TARGET_LANGUAGE_IDS";
+
+	public static final String SOURCE_INFO_ITEM_FIELD_VALUES =
+		"SOURCE_INFO_ITEM_FIELD_VALUES";
+
+	public static final String SOURCE_LANGUAGE_ID = "SOURCE_LANGUAGE_ID";
+
+	public static final String TARGET_INFO_ITEM_FIELD_VALUES =
+		"TARGET_INFO_ITEM_FIELD_VALUES";
+
+	public static final String TARGET_LANGUAGE_ID = "TARGET_LANGUAGE_ID";
+
+}
