7eefa5782de1c19af5993abafaa2d8b75a86d907
Alicia Garcia <alicia.garcia@liferay.com>
2021-01-13 14:28:59 +0100

    LPS-125442 when the dataDefinitionId is used, the link on DDMStructureLink between the ddm and the fileentry is not needed and this entry is preventing the deletion of the fileEntryType when it is not used anymore

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/upgrade/DLServiceUpgrade.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/upgrade/DLServiceUpgrade.java
@@ -23,6 +23,7 @@ import com.liferay.document.library.internal.upgrade.v1_0_2.UpgradeDLFileShortcu
 import com.liferay.document.library.internal.upgrade.v1_1_0.UpgradeSchema;
 import com.liferay.document.library.internal.upgrade.v1_1_2.UpgradeDLFileEntryType;
 import com.liferay.document.library.internal.upgrade.v2_0_0.UpgradeCompanyId;
+import com.liferay.document.library.internal.upgrade.v3_2_0.UpgradeDDMStructureLink;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.store.Store;
 import com.liferay.portal.configuration.upgrade.PrefsPropsToConfigurationUpgradeHelper;
@@ -97,6 +98,8 @@ public class DLServiceUpgrade implements UpgradeStepRegistrator {
 				UpgradeSchema(),
 			new com.liferay.document.library.internal.upgrade.v3_2_0.
 				UpgradeDLFileEntryType());
+
+		registry.register("3.2.0", "3.3.0", new UpgradeDDMStructureLink());
 	}
 
 	@Reference
