b5f02caf6ff0fa9db9ccfa873d3e741f8d621f8c
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-05-19 13:26:14 +0200

    LPS-131706 Autogenerated

--- portal-kernel/src/com/liferay/portal/kernel/service/ImageLocalServiceWrapper.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/ImageLocalServiceWrapper.java
@@ -329,6 +329,10 @@ public class ImageLocalServiceWrapper
 		return _imageLocalService.updateImage(image);
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link #updateImage(long, long, byte[])}
+	 */
+	@Deprecated
 	@Override
 	public Image updateImage(long imageId, byte[] bytes)
 		throws com.liferay.portal.kernel.exception.PortalException {
@@ -336,6 +340,10 @@ public class ImageLocalServiceWrapper
 		return _imageLocalService.updateImage(imageId, bytes);
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link #updateImage(long, long, byte[], String, int, int, int)}
+	 */
+	@Deprecated
 	@Override
 	public Image updateImage(
 			long imageId, byte[] bytes, String type, int height, int width,
@@ -346,6 +354,10 @@ public class ImageLocalServiceWrapper
 			imageId, bytes, type, height, width, size);
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link #updateImage(long, long, File)}
+	 */
+	@Deprecated
 	@Override
 	public Image updateImage(long imageId, java.io.File file)
 		throws com.liferay.portal.kernel.exception.PortalException {
@@ -353,6 +365,10 @@ public class ImageLocalServiceWrapper
 		return _imageLocalService.updateImage(imageId, file);
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link #updateImage(long, long, InputStream)}
+	 */
+	@Deprecated
 	@Override
 	public Image updateImage(long imageId, java.io.InputStream inputStream)
 		throws com.liferay.portal.kernel.exception.PortalException {
@@ -360,6 +376,10 @@ public class ImageLocalServiceWrapper
 		return _imageLocalService.updateImage(imageId, inputStream);
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link #updateImage(long, long, InputStream, boolean)}
+	 */
+	@Deprecated
 	@Override
 	public Image updateImage(
 			long imageId, java.io.InputStream inputStream,
@@ -368,6 +388,48 @@ public class ImageLocalServiceWrapper
 
 		return _imageLocalService.updateImage(
 			imageId, inputStream, cleanUpStream);
+	}
+
+	@Override
+	public Image updateImage(long companyId, long imageId, byte[] bytes)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _imageLocalService.updateImage(companyId, imageId, bytes);
+	}
+
+	@Override
+	public Image updateImage(
+			long companyId, long imageId, byte[] bytes, String type, int height,
+			int width, int size)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _imageLocalService.updateImage(
+			companyId, imageId, bytes, type, height, width, size);
+	}
+
+	@Override
+	public Image updateImage(long companyId, long imageId, java.io.File file)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _imageLocalService.updateImage(companyId, imageId, file);
+	}
+
+	@Override
+	public Image updateImage(
+			long companyId, long imageId, java.io.InputStream inputStream)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _imageLocalService.updateImage(companyId, imageId, inputStream);
+	}
+
+	@Override
+	public Image updateImage(
+			long companyId, long imageId, java.io.InputStream inputStream,
+			boolean cleanUpStream)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _imageLocalService.updateImage(
+			companyId, imageId, inputStream, cleanUpStream);
 	}
 
 	@Override
