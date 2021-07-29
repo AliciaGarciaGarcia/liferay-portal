e5431215272540e9edd2876815985b470d4834de
Alicia Garcia <alicia.garcia@liferay.com>
2021-07-29 13:00:48 +0200

    LPS-135864 Use new URLs in export dropdown

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/util/LayoutActionDropdownItemsProvider.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/util/LayoutActionDropdownItemsProvider.java
@@ -39,6 +39,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
+import javax.portlet.PortletURL;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -276,6 +278,28 @@ public class LayoutActionDropdownItemsProvider {
 									BeanPropertiesUtil.getLong(
 										draftLayout, "plid",
 										layout.getPlid())));
+
+							PortletURL portletURL = PortletURLBuilder.create(
+								_translationURLProvider.getExportTranslationURL(
+									layout.getGroupId(),
+									PortalUtil.getClassNameId(
+										Layout.class.getName()),
+									layout.getPlid(),
+									RequestBackedPortletURLFactoryUtil.create(
+										_httpServletRequest))
+							).setRedirect(
+								PortalUtil.getCurrentURL(_httpServletRequest)
+							).setPortletResource(
+								() -> {
+									PortletDisplay portletDisplay =
+										_themeDisplay.getPortletDisplay();
+
+									return portletDisplay.getId();
+								}
+							).buildPortletURL();
+
+							dropdownItem.putData("url", portletURL.toString());
+
 							dropdownItem.setLabel(
 								LanguageUtil.get(
 									_httpServletRequest,
