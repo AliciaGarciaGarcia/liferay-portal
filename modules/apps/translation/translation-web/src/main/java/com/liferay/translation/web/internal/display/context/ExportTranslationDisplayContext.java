1ca849978f15096c5d021737eb41b887fa8d3a0c
Jorge González <jorge.gonzalez@liferay.com>
2021-09-10 12:15:01 +0200

    LPS-135864 Fill display context from the render command

--- modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/display/context/ExportTranslationDisplayContext.java
+++ modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/display/context/ExportTranslationDisplayContext.java
@@ -14,8 +14,60 @@
 
 package com.liferay.translation.web.internal.display.context;
 
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.translation.exporter.TranslationInfoItemFieldValuesExporterTracker;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Jorge González
  */
 public class ExportTranslationDisplayContext {
+
+	public ExportTranslationDisplayContext(
+		long classNameId, long classPK,
+		long groupId, HttpServletRequest httpServletRequest,
+		InfoItemServiceTracker infoItemServiceTracker,
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse, Object model,
+		String title,
+		TranslationInfoItemFieldValuesExporterTracker
+			translationInfoItemFieldValuesExporterTracker) {
+
+		_classNameId = classNameId;
+
+		_className = PortalUtil.getClassName(_classNameId);
+
+		_classPK = classPK;
+		_groupId = groupId;
+		_httpServletRequest = httpServletRequest;
+		_infoItemServiceTracker = infoItemServiceTracker;
+		_liferayPortletResponse = liferayPortletResponse;
+		_model = model;
+		_title = title;
+		_translationInfoItemFieldValuesExporterTracker =
+			translationInfoItemFieldValuesExporterTracker;
+
+		_themeDisplay = (ThemeDisplay)liferayPortletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	private final String _className;
+	private final long _classNameId;
+	private final long _classPK;
+	private final long _groupId;
+	private final HttpServletRequest _httpServletRequest;
+	private final InfoItemServiceTracker _infoItemServiceTracker;
+	private final LiferayPortletResponse _liferayPortletResponse;
+	private final Object _model;
+	private final ThemeDisplay _themeDisplay;
+	private final String _title;
+	private final TranslationInfoItemFieldValuesExporterTracker
+		_translationInfoItemFieldValuesExporterTracker;
+
 }
