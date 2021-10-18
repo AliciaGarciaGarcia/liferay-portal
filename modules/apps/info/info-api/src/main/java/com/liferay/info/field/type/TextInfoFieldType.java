128b966a6ba0a53611002ad9c331a27ce45ad8fd
Alicia Garcia <alicia.garcia@liferay.com>
2021-10-18 17:59:01 +0200

    LPS-140303 Create RICH_TEXT attribute to add a layer of knowledge on the creation of the infoItem

--- modules/apps/info/info-api/src/main/java/com/liferay/info/field/type/TextInfoFieldType.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/field/type/TextInfoFieldType.java
@@ -27,6 +27,9 @@ public class TextInfoFieldType implements InfoFieldType {
 	public static final Attribute<TextInfoFieldType, Boolean> MULTILINE =
 		new Attribute<>();
 
+	public static final Attribute<TextInfoFieldType, Boolean> RICH_TEXT =
+		new Attribute<>();
+
 	@Override
 	public String getName() {
 		return "text";
