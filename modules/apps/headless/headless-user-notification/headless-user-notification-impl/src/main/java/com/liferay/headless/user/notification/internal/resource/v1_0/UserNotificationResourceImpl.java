ebfd4d6ed746fdb0788b4116a758426c0eb143cb
Carlos Correa <carlos.correa@liferay.com>
2022-12-27 15:03:03 +0100

    LPS-171762 Add implementation and DTOConverter

--- modules/apps/headless/headless-user-notification/headless-user-notification-impl/src/main/java/com/liferay/headless/user/notification/internal/resource/v1_0/UserNotificationResourceImpl.java
+++ modules/apps/headless/headless-user-notification/headless-user-notification-impl/src/main/java/com/liferay/headless/user/notification/internal/resource/v1_0/UserNotificationResourceImpl.java
@@ -14,9 +14,19 @@
 
 package com.liferay.headless.user.notification.internal.resource.v1_0;
 
+import com.liferay.headless.user.notification.dto.v1_0.UserNotification;
+import com.liferay.headless.user.notification.internal.dto.v1_0.UserNotificationDTOConverter;
 import com.liferay.headless.user.notification.resource.v1_0.UserNotificationResource;
+import com.liferay.portal.kernel.model.UserNotificationEvent;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.service.UserNotificationEventLocalService;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,4 +38,40 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class UserNotificationResourceImpl
 	extends BaseUserNotificationResourceImpl {
+
+	@Override
+	public Page<UserNotification> getMyUserNotificationsPage(
+			String search, Filter filter, Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		return Page.of(
+			transform(
+				_userNotificationEventLocalService.getUserNotificationEvents(
+					contextUser.getUserId(), pagination.getStartPosition(),
+					pagination.getEndPosition()),
+				this::_toUserNotification),
+			pagination,
+			_userNotificationEventLocalService.getUserNotificationEventsCount(
+				contextUser.getUserId()));
+	}
+
+	private UserNotification _toUserNotification(
+			UserNotificationEvent userNotificationEvent)
+		throws Exception {
+
+		return _userNotificationDTOConverter.toDTO(
+			new DefaultDTOConverterContext(
+				contextAcceptLanguage.isAcceptAllLanguages(), null, null,
+				userNotificationEvent.getUserNotificationEventId(),
+				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
+				contextUser));
+	}
+
+	@Reference
+	private UserNotificationDTOConverter _userNotificationDTOConverter;
+
+	@Reference
+	private UserNotificationEventLocalService
+		_userNotificationEventLocalService;
+
 }
