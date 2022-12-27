b6c00e27d9c3a2b1c47c553f49e3d99e79520fc4
Carlos Correa <carlos.correa@liferay.com>
2022-12-27 15:03:03 +0100

    LPS-171762 Add implementation and DTOConverter

--- modules/apps/headless/headless-user-notification/headless-user-notification-impl/src/main/java/com/liferay/headless/user/notification/internal/resource/v1_0/UserNotificationResourceImpl.java
+++ modules/apps/headless/headless-user-notification/headless-user-notification-impl/src/main/java/com/liferay/headless/user/notification/internal/resource/v1_0/UserNotificationResourceImpl.java
@@ -14,9 +14,18 @@
 
 package com.liferay.headless.user.notification.internal.resource.v1_0;
 
+import com.liferay.headless.user.notification.dto.v1_0.UserNotification;
+import com.liferay.headless.user.notification.internal.dto.v1_0.UserNotificationDTOConverter;
 import com.liferay.headless.user.notification.resource.v1_0.UserNotificationResource;
+import com.liferay.portal.kernel.exception.NoSuchUserNotificationEventException;
+import com.liferay.portal.kernel.model.UserNotificationEvent;
+import com.liferay.portal.kernel.service.UserNotificationEventLocalService;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,4 +37,126 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class UserNotificationResourceImpl
 	extends BaseUserNotificationResourceImpl {
+
+	@Override
+	public UserNotification getMyUserNotification(Long userNotificationId)
+		throws Exception {
+
+		return _toUserNotification(
+			_getUserNotificationEvent(userNotificationId));
+	}
+
+	@Override
+	public Page<UserNotification> getMyUserNotificationsPage(
+			Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			transform(
+				_userNotificationEventLocalService.getUserNotificationEvents(
+					contextUser.getUserId(), pagination.getStartPosition(),
+					pagination.getEndPosition()),
+				this::_toUserNotification),
+			pagination,
+			_userNotificationEventLocalService.getUserNotificationEventsCount(
+				contextUser.getUserId()));
+	}
+
+	@Override
+	public Page<UserNotification> getMyUserNotificationsReadPage(
+			Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			transform(
+				_userNotificationEventLocalService.
+					getArchivedUserNotificationEvents(
+						contextUser.getUserId(), true,
+						pagination.getStartPosition(),
+						pagination.getEndPosition()),
+				this::_toUserNotification),
+			pagination,
+			_userNotificationEventLocalService.
+				getArchivedUserNotificationEventsCount(
+					contextUser.getUserId(), true));
+	}
+
+	@Override
+	public Page<UserNotification> getMyUserNotificationsUnreadPage(
+			Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			transform(
+				_userNotificationEventLocalService.
+					getArchivedUserNotificationEvents(
+						contextUser.getUserId(), false,
+						pagination.getStartPosition(),
+						pagination.getEndPosition()),
+				this::_toUserNotification),
+			pagination,
+			_userNotificationEventLocalService.
+				getArchivedUserNotificationEventsCount(
+					contextUser.getUserId(), false));
+	}
+
+	@Override
+	public void putMyUserNotificationRead(Long userNotificationId)
+		throws Exception {
+
+		UserNotificationEvent userNotificationEvent = _getUserNotificationEvent(
+			userNotificationId);
+
+		_userNotificationEventLocalService.updateUserNotificationEvent(
+			userNotificationEvent.getUuid(),
+			userNotificationEvent.getCompanyId(), true);
+	}
+
+	@Override
+	public void putMyUserNotificationUnread(Long userNotificationId)
+		throws Exception {
+
+		UserNotificationEvent userNotificationEvent = _getUserNotificationEvent(
+			userNotificationId);
+
+		_userNotificationEventLocalService.updateUserNotificationEvent(
+			userNotificationEvent.getUuid(),
+			userNotificationEvent.getCompanyId(), false);
+	}
+
+	private UserNotificationEvent _getUserNotificationEvent(
+			long userNotificationId)
+		throws Exception {
+
+		UserNotificationEvent userNotificationEvent =
+			_userNotificationEventLocalService.getUserNotificationEvent(
+				userNotificationId);
+
+		if (userNotificationEvent.getUserId() != contextUser.getUserId()) {
+			throw new NoSuchUserNotificationEventException();
+		}
+
+		return userNotificationEvent;
+	}
+
+	private UserNotification _toUserNotification(
+			UserNotificationEvent userNotificationEvent)
+		throws Exception {
+
+		return _userNotificationDTOConverter.toDTO(
+			new DefaultDTOConverterContext(
+				contextAcceptLanguage.isAcceptAllLanguages(), null, null,
+				userNotificationEvent.getUserNotificationEventId(),
+				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
+				contextUser),
+			userNotificationEvent);
+	}
+
+	@Reference
+	private UserNotificationDTOConverter _userNotificationDTOConverter;
+
+	@Reference
+	private UserNotificationEventLocalService
+		_userNotificationEventLocalService;
+
 }
