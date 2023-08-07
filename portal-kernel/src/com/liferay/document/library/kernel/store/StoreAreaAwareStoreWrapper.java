477d8c40563e1da724cba88ce4159eec8c95314e
Alicia García <alicia.garcia@liferay.com>
2023-08-07 13:23:22 +0200

    LPS-192636 delete the directory only if was copied

--- portal-kernel/src/com/liferay/document/library/kernel/store/StoreAreaAwareStoreWrapper.java
+++ portal-kernel/src/com/liferay/document/library/kernel/store/StoreAreaAwareStoreWrapper.java
@@ -60,12 +60,12 @@ public class StoreAreaAwareStoreWrapper implements Store {
 			StoreAreaProcessor storeAreaProcessor =
 				_storeAreaProcessorSupplier.get();
 
-			storeAreaProcessor.copyDirectory(
-				companyId, repositoryId, dirName, _SOURCE_STORE_AREAS,
-				StoreArea.DELETED);
+			if (storeAreaProcessor.copyDirectory(
+					companyId, repositoryId, dirName, _SOURCE_STORE_AREAS,
+					StoreArea.DELETED)) {
 
-			_registerDeleteDirectoryCallback(
-				companyId, repositoryId, dirName, store);
+				_registerDeleteDirectoryCallback(
+					companyId, repositoryId, dirName, store);
 			}
 		}
 		else {
