046a139d79f2a3fa38602bd56f990c8dce26255c
Alicia García <alicia.garcia@liferay.com>
2023-12-20 14:38:00 +0100

    LPS-204012 use fixture as first step

--- modules/test/playwright/tests/knowledge-base/knowledge-base.spec.js
+++ modules/test/playwright/tests/knowledge-base/knowledge-base.spec.js
@@ -13,47 +13,43 @@ export const test = mergeTests(
 	knowledgeBaseTest
 );
 
-test('Create and delete a Knowledge Base Article', async ({
-															  _apiHelpers, page
-														  }) => {
-	await page.goto('/');
+test(
+	'Create and delete a Knowledge Base Article',
+	async ({
+			   _apiHelpers, _knowledgeBaseHelper, page
+		   }) => {
+		await page.goto('/');
 
-	//this only affects that the portal-ext.properties has the FF active
-	await _apiHelpers.featureFlag.updateFeatureFlag('LPS-188058', 'false');
+		//this only affects that the portal-ext.properties has the FF active
+		await _apiHelpers.featureFlag.updateFeatureFlag('LPS-188058', 'false');
 
-	const openProductMenuVisible = await page.getByLabel(
-		'Open Product Menu').isVisible();
+		await _knowledgeBaseHelper.openAdmin();
 
-	if (openProductMenuVisible) {
-		await page.getByLabel('Open Product Menu').click();
-	}
+		await page.getByLabel('New').click();
+		await page.getByRole('menuitem', {name: 'Basic Article'}).click();
+		await page.getByPlaceholder('Untitled Article').fill('Test Article');
+		await page.frameLocator('iframe').getByRole('textbox').fill(
+			'test content');
+		await page.getByRole('button', {name: 'Publish'}).click();
 
-	await page.getByRole('menuitem', {name: 'Content & Data'}).click();
-	await page.getByRole('menuitem', {name: 'Knowledge Base'}).click();
-	await page.getByLabel('New').click();
-	await page.getByRole('menuitem', {name: 'Basic Article'}).click();
-	await page.getByPlaceholder('Untitled Article').fill('Test Article');
-	await page.frameLocator('iframe').getByRole('textbox').fill('test content');
-	await page.getByRole('button', {name: 'Publish'}).click();
-
-	await expect(page.locator(
-		'[id="_com_liferay_knowledge_base_web_portlet_AdminPortlet_kbObjects_1"]').getByRole(
-		'link', {name: 'Test Article'})).toBeVisible();
-	await page.locator(
-		'[id="_com_liferay_knowledge_base_web_portlet_AdminPortlet_kbObjects_1"]').getByLabel(
-		'Show Actions').click();
-	await page.once('dialog', dialog => {
-		console.log(`Dialog message: ${dialog.message()}`);
-		dialog.accept().catch(() => {
+		await expect(page.locator(
+			'[id="_com_liferay_knowledge_base_web_portlet_AdminPortlet_kbObjects_1"]').getByRole(
+			'link', {name: 'Test Article'})).toBeVisible();
+		await page.locator(
+			'[id="_com_liferay_knowledge_base_web_portlet_AdminPortlet_kbObjects_1"]').getByLabel(
+			'Show Actions').click();
+		await page.once('dialog', dialog => {
+			console.log(`Dialog message: ${dialog.message()}`);
+			dialog.accept().catch(() => {
+			});
 		});
-	});
-	await page.getByRole('menuitem', {name: 'Delete'}).click();
+		await page.getByRole('menuitem', {name: 'Delete'}).click();
 
-	await expect(page.getByRole(
-		'heading',
-		{name: 'Knowledge base is empty.'})).toBeVisible();
-	await page.close();
-});
+		await expect(page.getByRole(
+			'heading',
+			{name: 'Knowledge base is empty.'})).toBeVisible();
+		await page.close();
+	});
 
 test(
 	'Publish and delete with schedule menu',
