2846b2dbb92c4df40559bfd7571ea8069b5b9bef
Alicia García <alicia.garcia@liferay.com>
2023-12-20 14:31:40 +0100

    LPS-204012 create first fixture and helper

---
+++ modules/test/playwright/helpers/knowledgeBase/knowledgeBaseHelper.js
@@ -0,0 +1,28 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+export class KnowledgeBaseHelper {
+
+	constructor(page) {
+		this.page = page;
+	}
+
+	async openAdmin() {
+		await this.page.goto('/');
+
+		const openProductMenuVisible = await this.page.getByLabel('Open Product Menu').isVisible();
+
+		if (openProductMenuVisible) {
+			await this.page.getByLabel('Open Product Menu').click();
+		}
+
+		await this.page.getByRole('menuitem', { name: 'Content & Data' }).click();
+		await this.page.getByRole('menuitem', { name: 'Knowledge Base' }).click();
+
+		return {
+			page: this.page
+		};
+	}
+
+}
