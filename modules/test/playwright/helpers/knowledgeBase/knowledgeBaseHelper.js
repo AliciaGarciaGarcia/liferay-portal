1300b45c8b14afaa570117fcf56a618a5b52a713
Alicia García <alicia.garcia@liferay.com>
2023-12-20 16:51:46 +0100

    LPS-204012 use the new product menu page

--- modules/test/playwright/helpers/knowledgeBase/knowledgeBaseHelper.js
+++ modules/test/playwright/helpers/knowledgeBase/knowledgeBaseHelper.js
@@ -2,23 +2,18 @@
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
+import {ProductMenuPage} from '../../pages/product-navigation-product-menu/productMenu.page';
+
 export class KnowledgeBaseHelper {
 
 	constructor(page) {
+		this.productMenuPage = new ProductMenuPage(page);
 		this.page = page;
 	}
 
 	async openAdmin() {
-		await this.page.goto('/');
-
-		const openProductMenuVisible = await this.page.getByLabel('Open Product Menu').isVisible();
-
-		if (openProductMenuVisible) {
-			await this.page.getByLabel('Open Product Menu').click();
-		}
-
-		await this.page.getByRole('menuitem', { name: 'Content & Data' }).click();
-		await this.page.getByRole('menuitem', { name: 'Knowledge Base' }).click();
+		await this.productMenuPage.goToKnowledgeBase();
 	}
 
 }
