23b2becc654207db919d371497531e4335abef61
Alicia García <alicia.garcia@liferay.com>
2024-01-15 13:21:13 +0100

    LPS-204012 create pages to different spaces

---
+++ modules/test/playwright/pages/knowledge-base-web/KnowledgeBaseFoldersAndArticles.page.ts
@@ -0,0 +1,82 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {FrameLocator, Locator, Page} from '@playwright/test';
+
+import {KnowledgeBasePage} from './KnowledgeBase.page';
+import {KnowledgeBaseEditArticlePage} from './KnowledgeBaseEditArticle.page';
+import {KnowledgeBaseViewArticlePage} from './KnowledgeBaseViewArticle.page';
+
+export class KnowledgeBaseFoldersAndArticlesPage extends KnowledgeBasePage {
+	readonly basicArticleMenuItem: Locator;
+	readonly newButton: Locator;
+	readonly page: Page;
+	knowledgeBaseEditArticlePage: KnowledgeBaseEditArticlePage;
+	knowledgeBaseViewArticlePage: KnowledgeBaseViewArticlePage;
+
+	constructor(page: Page) {
+		super(page);
+		this.basicArticleMenuItem = page.getByRole('menuitem', {
+			name: 'Basic Article',
+		});
+
+		this.newButton = page.getByLabel('New');
+		this.knowledgeBaseEditArticlePage = new KnowledgeBaseEditArticlePage(
+			page
+		);
+		this.knowledgeBaseViewArticlePage = new KnowledgeBaseViewArticlePage(
+			page
+		);
+	}
+
+	async publishNewKnowledgeBaseArticle(content: string, title: string) {
+		await this.goToNewArticle();
+		await this.knowledgeBaseEditArticlePage.publishNewKnowledgeBaseArticle(
+			content,
+			title
+		);
+	}
+
+	async publishNewKnowledgeBaseArticleWithSchedule(
+		content: string,
+		title: string
+	) {
+		await this.goToNewArticle();
+		await this.knowledgeBaseEditArticlePage.publishNewKnowledgeBaseArticleWithSchedule(
+			content,
+			title
+		);
+	}
+
+	private async goToNewArticle() {
+		await this.goToFoldersAndArticles();
+		await this.newButton.click();
+		await this.basicArticleMenuItem.click();
+	}
+
+	async deleteKnowledgeBaseArticle(page: Page, title: string) {
+
+		// const regex: RegExp = /(?=.*${title})/g;
+		// const regex: RegExp = new RegExp("/^\s*"+title+"\s*$/i", "g");
+		// const regex: RegExp = new RegExp('/^'+title+'\s+$/i');
+		//
+		// //const article = page.getByLabel(/^\s*${title}\s*$/i);
+		// const article = page.getByLabel(new RegExp(title+'\s+$/i'));
+		//  if(await article.isVisible()){
+		//  	await article.check();
+		// 	 page.once('dialog', (dialog) => {
+		// 		 console.log(`Dialog message: ${dialog.message()}`);
+		// 		 dialog.accept().catch(() => {});
+		// 	 });
+		// 	await page.getByRole('button', { name: 'Delete' }).click();
+		//  }
+
+		await page.getByRole('link', {name: title}).click();
+
+		await this.knowledgeBaseViewArticlePage.deleteKnowledgeBaseArticle(
+			page
+		);
+	}
+}
---
+++ modules/test/playwright/pages/knowledge-base-web/KnowledgeBaseViewArticle.page.ts
@@ -0,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Locator, Page} from '@playwright/test';
+
+export class KnowledgeBaseViewArticlePage {
+	readonly page: Page;
+	showActionsButton: Locator;
+	deleteMenuItem: Locator;
+
+	constructor(page: Page) {
+		//this.deleteMenuItem = page.getByRole('menuitem', {name: 'Delete'});
+
+		this.page = page;
+
+		// this.showActionsButton = this.page
+		// 	.locator(
+		// 		'#portlet_com_liferay_knowledge_base_web_portlet_AdminPortlet'
+		// 	)
+		// 	.getByRole('list')
+		// 	.locator('div')
+		// 	.nth(1);
+	}
+
+	async deleteKnowledgeBaseArticle(page: Page) {
+		this.showActionsButton = this.page
+			.locator(
+				'#portlet_com_liferay_knowledge_base_web_portlet_AdminPortlet'
+			)
+			.getByRole('list')
+			.locator('div')
+			.nth(1);
+
+		await page.getByLabel('Show Actions').last().click();	
+		await this.showActionsButton.click();
+		await page.getByRole('menuitem', {name: 'Delete'}).click();
+	}
+}
