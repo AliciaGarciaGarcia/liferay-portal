5d961701b6562afb22d167c40fe9e01ee9ab1fea
Jorge González <jorge.gonzalez@liferay.com>
2024-01-23 11:56:57 +0100

    LPS-204012 Use the correct feature flag and method

--- modules/test/playwright/tests/knowledge-base-web/knowledge-base-web.spec.ts
+++ modules/test/playwright/tests/knowledge-base-web/knowledge-base-web.spec.ts
@@ -83,14 +83,12 @@ test('KBArticle - Delete all - without recycle Bin', async ({
 	knowledgeBasePage,
 	page,
 }) => {
-	await apiHelpers.featureFlag.updateFeatureFlag('LPS-188058', true);
+	await apiHelpers.featureFlag.updateFeatureFlag('LPS-188058', false);
 
-	await knowledgeBasePage.publishNewKnowledgeBaseArticleWithSchedule(
+	await knowledgeBasePage.publishNewKnowledgeBaseArticle(
 		getRandomString(),
 		getRandomString()
 	);
-
-	await apiHelpers.featureFlag.updateFeatureFlag('LPS-188058', false);
 
 	await knowledgeBasePage.deleteAll(page, false);
 
