7a9f7d3dbc74ad416bd18c16cfaee2531cf3788e
Alicia García <alicia.garcia@liferay.com>
2024-11-21 11:25:39 +0100

    LPD-42445 as on the updateAssetEntry, the serviceContext can be null

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLEntryLocalServiceImpl.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLEntryLocalServiceImpl.java
@@ -881,6 +881,10 @@ public class FriendlyURLEntryLocalServiceImpl
 			Map<String, String> urlTitleMap, ServiceContext serviceContext)
 		throws PortalException {
 
+		if (serviceContext == null) {
+			return;
+		}
+
 		long[] friendlyURLAssetCategoryIds = GetterUtil.getLongValues(
 			serviceContext.getAttribute("friendlyURLAssetCategoryIds"));
 
