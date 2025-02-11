b9def3891265e7ef5d729f0ed7c6e26d093fd373
Alicia García <alicia.garcia@liferay.com>
2025-02-11 14:19:27 +0100

    LPD-43166 buildService

--- modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectEntryModelImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectEntryModelImpl.java
@@ -154,26 +154,32 @@ public class ObjectEntryModelImpl
 	 * @deprecated As of Athanasius (7.3.x), replaced by {@link #getColumnBitmask(String)}
 	 */
 	@Deprecated
-	public static final long STATUS_COLUMN_BITMASK = 32L;
+	public static final long OBJECTENTRYFOLDERID_COLUMN_BITMASK = 32L;
 
 	/**
 	 * @deprecated As of Athanasius (7.3.x), replaced by {@link #getColumnBitmask(String)}
 	 */
 	@Deprecated
-	public static final long USERID_COLUMN_BITMASK = 64L;
+	public static final long STATUS_COLUMN_BITMASK = 64L;
 
 	/**
 	 * @deprecated As of Athanasius (7.3.x), replaced by {@link #getColumnBitmask(String)}
 	 */
 	@Deprecated
-	public static final long UUID_COLUMN_BITMASK = 128L;
+	public static final long USERID_COLUMN_BITMASK = 128L;
+
+	/**
+	 * @deprecated As of Athanasius (7.3.x), replaced by {@link #getColumnBitmask(String)}
+	 */
+	@Deprecated
+	public static final long UUID_COLUMN_BITMASK = 256L;
 
 	/**
 	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
 	 *		#getColumnBitmask(String)}
 	 */
 	@Deprecated
-	public static final long OBJECTENTRYID_COLUMN_BITMASK = 256L;
+	public static final long OBJECTENTRYID_COLUMN_BITMASK = 512L;
 
 	/**
 	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
@@ -680,6 +686,16 @@ public class ObjectEntryModelImpl
 		}
 
 		_objectEntryFolderId = objectEntryFolderId;
+	}
+
+	/**
+	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
+	 *             #getColumnOriginalValue(String)}
+	 */
+	@Deprecated
+	public long getOriginalObjectEntryFolderId() {
+		return GetterUtil.getLong(
+			this.<Long>getColumnOriginalValue("objectEntryFolderId"));
 	}
 
 	@JSON
