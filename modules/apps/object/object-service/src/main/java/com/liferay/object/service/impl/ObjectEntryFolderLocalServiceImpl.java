4bfd92c48fa4722d3f001cafd73fee144ec8937b
Alicia García <alicia.garcia@liferay.com>
2025-03-26 12:19:49 +0100

    LPD-47621 deleteWorkflowDefinitionLink when deleting the ObjectEntryFolder

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryFolderLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryFolderLocalServiceImpl.java
@@ -197,6 +197,12 @@ public class ObjectEntryFolderLocalServiceImpl
 			objectEntryFolder.getGroupId(), objectEntryFolder.getCompanyId(),
 			objectEntryFolder.getTreePath() + "%");
 
+		_workflowDefinitionLinkLocalService.deleteWorkflowDefinitionLink(
+			objectEntryFolder.getCompanyId(), objectEntryFolder.getGroupId(),
+			ObjectEntryFolder.class.getName(),
+			objectEntryFolder.getObjectEntryFolderId(),
+			ObjectDefinitionConstants.OBJECT_DEFINITION_ID_ALL);
+
 		return objectEntryFolder;
 	}
 
