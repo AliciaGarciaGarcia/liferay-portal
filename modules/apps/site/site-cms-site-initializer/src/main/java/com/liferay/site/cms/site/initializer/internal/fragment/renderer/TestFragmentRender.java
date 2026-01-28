4e9af5e25d82a9faf1cf2ac084fc41d06b4b7093
Alicia García <alicia.garcia@liferay.com>
2026-01-28 14:34:59 +0100

    wip

---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/TestFragmentRender.java
@@ -0,0 +1,61 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.fragment.renderer;
+
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererContext;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(service = FragmentRenderer.class)
+public class TestFragmentRender extends BaseSectionFragmentRenderer {
+
+	@Override
+	public String getCollectionKey() {
+		return "Test";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return "test component";
+	}
+
+	@Override
+	public boolean isSelectable(HttpServletRequest httpServletRequest) {
+		return true;
+	}
+
+	@Override
+	public void render(
+			FragmentRendererContext fragmentRendererContext,
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		try {
+			PrintWriter printWriter = httpServletResponse.getWriter();
+
+			printWriter.write("<div><span aria-hidden=\"true\" class=\"");
+			printWriter.write("loading-animation\">hello</span>");
+			printWriter.write("</div>");
+		}
+		catch (IOException | RuntimeException exception) {
+			throw exception;
+		}
+		catch (Exception exception) {
+			throw new IOException(exception);
+		}
+	}
+
+}
