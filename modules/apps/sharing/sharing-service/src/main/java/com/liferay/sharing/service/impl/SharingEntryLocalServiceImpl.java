ce3750e6dd90c57edee6daeb2f701391b3cabccc
Alicia García <alicia.garcia@liferay.com>
2026-05-04 10:31:21 +0200

    LPD-88003 add methods, to deletes by toTicketId and ToUserGroupId. To search by ToUserGroupId

--- modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/service/impl/SharingEntryLocalServiceImpl.java
+++ modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/service/impl/SharingEntryLocalServiceImpl.java
@@ -343,6 +343,26 @@ public class SharingEntryLocalServiceImpl
 				externalReferenceCode, groupId));
 	}
 
+	@Override
+	public void deleteToTicketSharingEntries(long toTicketId) {
+		List<SharingEntry> sharingEntries =
+			sharingEntryPersistence.findByToTicketId(toTicketId);
+
+		for (SharingEntry sharingEntry : sharingEntries) {
+			sharingEntryLocalService.deleteSharingEntry(sharingEntry);
+		}
+	}
+
+	@Override
+	public void deleteToUserGroupSharingEntries(long toUserGroupId) {
+		List<SharingEntry> sharingEntries =
+			sharingEntryPersistence.findByToUserGroupId(toUserGroupId);
+
+		for (SharingEntry sharingEntry : sharingEntries) {
+			sharingEntryLocalService.deleteSharingEntry(sharingEntry);
+		}
+	}
+
 	/**
 	 * Deletes the sharing entries for resources shared with the user.
 	 *
@@ -532,6 +552,11 @@ public class SharingEntryLocalServiceImpl
 	@Override
 	public List<SharingEntry> getToTicketSharingEntries(long toTicketId) {
 		return sharingEntryPersistence.findByToTicketId(toTicketId);
+	}
+
+	@Override
+	public List<SharingEntry> getToUserGroupSharingEntries(long toUserGroupId) {
+		return sharingEntryPersistence.findByToUserGroupId(toUserGroupId);
 	}
 
 	/**
